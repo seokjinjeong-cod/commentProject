259f62815b3b2ac53909b5fd90852bd915ca8e2d
admin <admin@DESKTOP-RAE3LDC>
2021-10-08 13:15:57 +0900

    commit

--- commentProject/src/co/yedam/comment/CommentDAO.java
+++ commentProject/src/co/yedam/comment/CommentDAO.java
@@ -40,9 +40,49 @@ public class CommentDAO extends DAO {
 		return list;
 	}
 
-	// 글입력..
-	public void insertComment() {
-
+	// 글입력.. 1)현재id 2)id+1 값으로 글등록 3)현재id값 변경
+	public Comment insertComment(Comment comment) {
+		connect();
+		int currId = 0;
+		try {
+			conn.setAutoCommit(false);
+		} catch (SQLException e) {
+			e.printStackTrace();
+		}
+		try {
+			stmt = conn.createStatement();
+			rs = stmt.executeQuery("select value from id_repository where name='COMMENT'");
+			if(rs.next()) {
+				currId = rs.getInt("value");
+			}
+			currId++;
+			psmt = conn.prepareStatement("insert into comments values(?,?,?)");
+			psmt.setInt(1, currId);
+			psmt.setString(2, comment.getName());
+			psmt.setString(3, comment.getContent());
+			int r = psmt.executeUpdate();
+			System.out.println(r + "건 입력");
+			
+			psmt = conn.prepareStatement("update id_repository set value=? where name='COMMENT'");
+			psmt.setInt(1, currId);
+			r = psmt.executeUpdate();
+			System.out.println(r + "건 변경.");
+			
+			conn.commit();
+			comment.setId(String.valueOf(currId));
+			return comment;
+			
+		} catch (SQLException e) {
+			e.printStackTrace();
+			try {
+				conn.rollback();
+			} catch (SQLException e1) {
+				e1.printStackTrace();
+			}
+			return null;
+		} finally {
+			disconnect();
+		}
 	}
 
 	// 글수정..
--- commentProject/src/co/yedam/comment/CommentServlet.java
+++ commentProject/src/co/yedam/comment/CommentServlet.java
@@ -23,11 +23,16 @@ public class CommentServlet extends HttpServlet {
 	protected void doGet(HttpServletRequest request, HttpServletResponse response)
 			throws ServletException, IOException {
 		
+		request.setCharacterEncoding("UTF-8");
+		response.setCharacterEncoding("UTF-8");
 		response.setContentType("text/html; charset=UTF-8");
 		
-		CommentDAO dao = CommentDAO.getInstance();
 		PrintWriter out = response.getWriter();
+		Gson gson = new GsonBuilder().create();
+		
 		String cmd = request.getParameter("cmd");
+		CommentDAO dao = CommentDAO.getInstance();
+		
 		if(cmd == null) {
 			System.out.println("<h1>빈페이지입니다</h1>");
 			dao.getCommentList();
@@ -35,20 +40,31 @@ public class CommentServlet extends HttpServlet {
 		} else if(cmd.equals("list")){
 			System.out.println("<h1>리스트페이지입니다.</h1>");
 			List<Comment> list = dao.getCommentList();
-			Gson gson = new GsonBuilder().create();
 			out.println(gson.toJson(list));
 			
 		} else if(cmd.equals("add")){
-			out.println("<h1>추가페이지입니다.</h1>");
+			System.out.println("<h1>추가페이지입니다.</h1>");
+			String name = request.getParameter("name");
+			String content = request.getParameter("content");
+			
+			Comment comment = new Comment();
+			comment.setName(name);
+			comment.setContent(content);
+			
+			dao.insertComment(comment);
+			
+			System.out.println(gson.toJson(comment));
+			
+			out.println(gson.toJson(comment));
 			
 		} else if(cmd.equals("mod")){
-			out.println("<h1>수정페이지입니다.</h1>");
+			System.out.println("<h1>수정페이지입니다.</h1>");
 			
 		} else if(cmd.equals("del")){
-			out.println("<h1>삭제페이지입니다.</h1>");
+			System.out.println("<h1>삭제페이지입니다.</h1>");
 			
 		} else {
-			out.println("<h1>" + cmd + "</h1>");
+			System.out.println("<h1>" + cmd + "</h1>");
 		}
 	}
 
